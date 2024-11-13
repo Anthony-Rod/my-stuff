a0c7a6fec23c889e2562475447b63fc27d39f4b0
Anthony R <91355240+Anthony-Rod@users.noreply.github.com>
2024-11-13 08:45:19 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,35 @@
-import cv2
+import cv2 as cv
+import numpy as np
+from matplotlib import pyplot as plt
+import os
+import time
 
-img = cv2.imread("/images/tomato.png", cv2.IMREAD_COLOR)
+img = cv.imread("opencv_image/images/eminem.jpg")
+img2 = img
+W_SIZE = 4
+H_SIZE = 4
 
-cv2.imshow("image", img)
+height, width, channels = img.shape
 
-cv2.waitKey(0)
+for ih in range(H_SIZE ):
+   for iw in range(W_SIZE ):
+   
+      x = width/W_SIZE * iw 
+      y = height/H_SIZE * ih
+      h = (height / H_SIZE)
+      w = (width / W_SIZE )
+      print(x,y,h,w)
+      img = img[int(y):int(y+h), int(x):int(x+w)]
+      NAME = str(time.time()) 
+      cv.imwrite("opencv_image/" + str(ih)+str(iw) +  ".jpg", img)
+      img = img2
+      cv.imshow("img", img2)
+#gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 
-cv2.destroyAllWindows()
+#ret, thresh = cv.threshold(gray, 127, 255, 0)
 
+#contours, hierarchy = cv.findContours(thresh, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+
+cv.waitKey(0)
+
+cv.destroyAllWindows()
